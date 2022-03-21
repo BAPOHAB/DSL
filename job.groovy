b7d441b1844dd8143a0faf3f648af5b6003bdef5
Viktar Varona <viktar_varona@epam.com>
2022-03-21 18:44:52 +0300

    job.groovy fixed

--- job.groovy
+++ job.groovy
@@ -1,8 +1,5 @@
-def project = 'BAPOHAB/DSL'
-def branchApi = new URL("https://api.github.com/repos/${project}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 freeStyleJob('HW6/MNTLAB-{student}-main-build-job') {
     steps {
-        echo "Hello from ${branches}"
+        echo "Hello from"
     }
 }
