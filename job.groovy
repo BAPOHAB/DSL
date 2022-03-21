6f27c2b1a53999c3db12d765ded964ad9282ef22
Viktar Varona <viktar_varona@epam.com>
2022-03-21 18:30:03 +0300

    job.groovy fixed

--- job.groovy
+++ job.groovy
@@ -1,8 +1,7 @@
-def jobName
+
 def i = [1, 2, 3, 4]
 i.each {
-    jobName = "HW6/MNTLAB-vvarona-child${i}-build-job"
-    freeStyleJob(jobName) {
+    freeStyleJob('jobName') {
         steps {
             echo "Hello from ${jobName}"
         }
