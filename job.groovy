613ee217e24b6d153c25ff4a3022b06fcbda2180
Viktar Varona <viktar_varona@epam.com>
2022-03-21 16:35:05 +0300

    job.groovy fixed

--- job.groovy
+++ job.groovy
@@ -1,15 +1,17 @@
-def project = 'quidryan/aws-sdk-test'
+def project = 'BAPOHAB/DSL'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+def i = 1
 branches.each {
     def branchName = it.name
-    def jobName = "${project}-${branchName}".replaceAll('/','-')
+    def jobName = "HW6/MNTLAB-vvarona-child${i}-build-job"
     freeStyleJob(jobName) {
         scm {
             git("git://github.com/${project}.git", branchName)
         }
         steps {
-            maven("test -Dproject.name=${project}/${branchName}")
+            echo "Hello from ${branchName}"
         }
+    i += 1
     }
 }
