e15c773b1c23f3654fa21c7d2e88dbdd7e4064f3
Viktar Varona <viktar_varona@epam.com>
2022-03-22 14:01:40 +0300

    job.groovy fixed

--- job.groovy
+++ job.groovy
@@ -1,4 +1,4 @@
-def project = 'BAPOHAB/DSL'
+/*def project = 'BAPOHAB/DSL'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 def i = 1
@@ -10,5 +10,24 @@ branches.each {
             shell('echo "Hello from ${branchName}"')
         }
     i += 1
+    }
+}*/
+
+
+job('HW6/MNTLAB-vvarona-main-build-job') {
+    description('Main job')
+    parameters {
+        stringParam('BRANCH', 'main', 'Branch choosing')
+        activeChoiceParam('CHOICE-1') {
+            description('Job choosing')
+            choiceType('CHECKBOX')
+            groovyScript {
+                script(
+                    for (int i=1; i<5; i++) {
+                        def jobName = "HW6/MNTLAB-vvarona-child${i}-build-job"
+                    }
+                )
+            }
+        }
     }
 }
